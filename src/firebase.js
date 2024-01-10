1010c2aefd1ba9619df416c75518d0b95238ddfa
Imrancu <mdimran3772@gmail.com>
2024-01-10 16:36:19 +0600

    completed createlisting page functionality with hangling possibel error in front end

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjlAzBeowZZvb5X6Go0K7T9R2GX8R3FYM",
+  authDomain: "realestate-830ed.firebaseapp.com",
+  projectId: "realestate-830ed",
+  storageBucket: "realestate-830ed.appspot.com",
+  messagingSenderId: "215768284918",
+  appId: "1:215768284918:web:a9869695fdec427a7ff6d7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
