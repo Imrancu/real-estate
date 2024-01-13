5435e8eb2949eaa7004ebf1598e801b12c8c8b21
Imrancu <mdimran3772@gmail.com>
2024-01-13 19:38:16 +0600

    added some info

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://api-self-zeta.vercel.app',
         secure: false,
+        changeOrigin: true,
+        // Add the line below to specify the SSL/TLS version
+        // e.g., 'https://api-self-zeta.vercel.app': { target: 'https://api-self-zeta.vercel.app', secure: false, protocol: 'http2' },
       },
     },
   },
   plugins: [react()],
-})
+});
