c174a1a3a22dbb3723cb74997a12d8ea3cf178ef
Imrancu <mdimran3772@gmail.com>
2024-01-17 21:34:03 +0600

    solved error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+
+
+const env = loadEnv(
+    'all',
+    process.cwd()
+);
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_REACT_APP_SERVER_API,
+        target: env.VITE_REACT_APP_SERVER_API,
         // secure: false,
         // changeOrigin: true,
         // Add the line below to specify the SSL/TLS version
